b1c659af93e52004f31585dedeccbbc8901cb1c1
GasKitela <gas.kitela@gmail.com>
2020-03-10 12:34:17 +0100

    removing fail test

--- test/services/UserServiceTest.scala
+++ test/services/UserServiceTest.scala
@@ -14,10 +14,6 @@ class UserServiceTest extends FlatSpec with BeforeAndAfter with BeforeAndAfterAl
 
   }
 
-  "Sarasa" should "be true" in {
-    false shouldBe true
-  }
-
   "Attempt to create user when there is none" should "be successful" in {
     userService.createInstanceUser(instanceUserRequest) shouldBe Right()
   }
